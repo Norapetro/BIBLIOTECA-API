91e64bd408d32e36f17e2c764749e35dde4e3c67
Nora <adrianaguirrepetro@gmail.com>
2024-03-19 14:53:21 -0500

    se eliminan datos no necesarios

--- src/Components/BookDetails.jsx
+++ src/Components/BookDetails.jsx
@@ -6,13 +6,12 @@ const BookDetails = ({ book }) => {
     <div className="card">
       <img src=".AQUI VA LA IMAGEN DEL LIBRO." className="card-img-top" alt="Book Image" />
       <div className="card-body">
-        <h5 className="card-title">{book.title}</h5>
+        <p className="card-text"><strong>Id:</strong> {book.id}</p>
         <p className="card-text"><strong>Author:</strong> {book.author}</p>
         <p className="card-text"><strong>Tematica:</strong> {book.tematica}</p>
         <p className="card-text"><strong>Publication Date:</strong> {book.publication_date}</p>
         <p className="card-text"><strong>Publisher:</strong> {book.publisher}</p>
         <p className="card-text"><strong>Number of Pages:</strong> {book.num_pages}</p>
-        <p className="card-text"><strong>Price:</strong> {book.price}</p>
         <p className="card-text"><strong>State:</strong> {book.state}</p>
         <Link to="/booktransaction" className="btn btn-primary">Book Transaction</Link>
       </div>
@@ -23,13 +22,12 @@ const BookDetails = ({ book }) => {
 // Definir la validación de los props
 BookDetails.propTypes = {
   book: PropTypes.shape({
-    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
     author: PropTypes.string.isRequired,
     tematica: PropTypes.string.isRequired,
     publication_date: PropTypes.string.isRequired,
     publisher: PropTypes.string.isRequired,
     num_pages: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
     state: PropTypes.string.isRequired,
   }).isRequired,
 };
--- src/Components/BookTransaction.jsx
+++ src/Components/BookTransaction.jsx
@@ -1,17 +1,26 @@
 // PRESTAMO Y DEVOLUCIONES DE LIBROS
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const BookTransaction = () => {
   const [transaction, setTransaction] = useState({
-    user_id: "",
-    username: "",
     book_id: "",
     book_title: "",
+    user_id: "",
+    username: "",
     date_transaction: "",
     type_transaction: "",
   });
+
+  useEffect(() => {
+    const currentDate = new Date().toISOString().slice(0, 10);
+    setTransaction(prevTransaction => ({
+      ...prevTransaction,
+      date_transaction: currentDate,
+    }));
+  }, []); // Dependencia vacía para que se ejecute solo una vez al montar el componente
+  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,24 +48,6 @@ const BookTransaction = () => {
       <h2>Book Transaction (Prestamo de Libros)</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>User ID:</label>
-          <input
-            type="text"
-            name="user_id"
-            value={transaction.user_id}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>User Name:</label>
-          <input
-            type="text"
-            name="username"
-            value={transaction.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
           <label>Book ID:</label>
           <input
             type="text"
@@ -75,12 +66,31 @@ const BookTransaction = () => {
           />
         </div>
         <div>
+          <label>User ID:</label>
+          <input
+            type="text"
+            name="user_id"
+            value={transaction.user_id}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>User Name:</label>
+          <input
+            type="text"
+            name="username"
+            value={transaction.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
           <label>Date Transaction:</label>
           <input
             type="text"
             name="date_transaction"
             value={transaction.date_transaction}
             onChange={handleChange}
+            readOnly // Hace que el campo sea de solo lectura
           />
         </div>
         <div>
